8b32fd3e1d6c9bb9132fe6201335772e259e2467
Hrisi <hristinaih@gmail.com>
2013-06-13 20:18:38 +0300

    Adding subdivision.

--- Bezier_curves.py
+++ Bezier_curves.py
@@ -7,6 +7,8 @@ class BezierCurves:
         self.curve_points = []
         self.derivative_control_points = []
         self.derivative_points = dict()
+        self.subdiv_points_first = dict()
+        self.subdiv_points_last = dict()
 
     def append_point(self, point):
         if len(self.control_points) == self._degree + 1:
@@ -18,11 +20,19 @@ class BezierCurves:
         algr_step = []
         algr_step.append([point for point in points])
         degree = len(points)
+        subdiv_last = []
+        self.subdiv_points_first[param] = []
+        self.subdiv_points_last[param] = []
 
         for step in range(1, degree):
             algr_step.append([(1 - param) * algr_step[step - 1][point] +
                              param * algr_step[step - 1][point + 1]
                              for point in range(len(algr_step[step - 1]) - 1)])
+
+            self.subdiv_points_first[param].append(algr_step[step - 1][0])
+            subdiv_last.append(algr_step[step - 1][degree - step])
+
+        self.subdiv_points_last[param] = subdiv_last.reverse()
 
         return algr_step[degree - 1][0]
 
@@ -39,6 +49,7 @@ class BezierCurves:
     def curve_calculation(self, control_points, derivative=None):
         key = self._degree - len(control_points) + 1
         self.derivative_points[key] = []
+        curve_points = []
 
         for t in range(self.RANGE_STEP + 1):
             param = t / self.RANGE_STEP
