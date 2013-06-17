a2168ae31363463279f2cd4084db32dc98b77078
Hrisi <hristinaih@gmail.com>
2013-06-17 11:08:18 +0300

    Dividing Bezier points and deBoor points into two functions.

--- SplinesC1.py
+++ SplinesC1.py
@@ -7,32 +7,38 @@ class SplineC1:
     def __init__(self, degree, interval):
 
         if (degree <= 1):
-            raise IndexError(self.NONE_EXISTING_SPLINE)
+            raise (self.NONE_EXISTING_SPLINE)
 
         self.spline = SplinesC0.SplineC0(degree, interval)
         self.deBoor_points = []
-        self.counter = 0
+        self.partial_curve_counter = 0
 
-    def append_point(self, point):
+    def append_deBoor_point(self, point):
         if len(self.deBoor_points) == 2 * len(self.spline._degree):
             raise IndexError
 
-        len_points = len(self.spline.control_points)
-        if len_points % self.spline._degree == 0:
-            self.counter += 1
-            denominator = (self.spline.interval[count - 1] +
-                           self.spline.interval[count])
-            numerator = (self.spline.interval[count] *
-                         self.spline.control_points[len_points - 1] +
-                         self.spline.interval[count] *
-                         self.spline.control_points[count - 1])
-
-            calculated_point = numerator / denominator
-
-            self.spline.append_point(calculated_point)
-
-        self.spline.append_point(point)
         self.deBoor_points.append(point)
 
+    def _append_Bezier_points(self):
+        counter = self.partial_curve_counter
+        len_points = self.spline.counts_count
+        for count_point in range(0, len_points):
+            if i % self.spline._degree == 0 and count_point < len_points - 1:
+                counter += 1
+                denominator = (self.spline.interval[counter - 1] +
+                               self.spline.interval[counter])
+                numerator = (self.spline.interval[counter] *
+                             self.deBoor_points[count_point - 1] +
+                             self.spline.interval[count] *
+                             self.deBoor_points[count_point - 1])
+
+                calculated_point = numerator / denominator
+                self.spline.append_point(calculated_point)
+
+                continue
+
+            self.spline.append_point(self.deBoor_points[count_point])
+
     def draw_spline(self):
+        self._append_Bezier_points()
         self.spline.draw()
