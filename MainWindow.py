e7fc2474a0884a4fdab2e5b02482eb5db489286f
Hrisi <hristinaih@gmail.com>
2013-06-22 23:14:57 +0300

    Adding improvement to GUI.

--- MainWindow.py
+++ MainWindow.py
@@ -1,6 +1,8 @@
 from PyQt4 import QtCore, QtGui, QtOpenGL
 import sys
 import Workspace
+import Bezier_curves
+import Vec3D
 
 
 class MainWindow(QtGui.QMainWindow):
@@ -10,23 +12,81 @@ class MainWindow(QtGui.QMainWindow):
         self.environment()
         self.window_properties()
 
+        #value, accepted = QtGui.QInputDialog.getInt(None, "foo", "bar")
+
         self.show()
+        #test
+        curve = Bezier_curves.BezierCurve()
+
+        points = [Vec3D.Vec3D(0, 0, 0),
+                  Vec3D.Vec3D(0, 1, 0),
+                  Vec3D.Vec3D(2, 1, 0),
+                  Vec3D.Vec3D(3, 0, 0),
+                  Vec3D.Vec3D(4, -1, 0),
+                  Vec3D.Vec3D(2, -1, 0)]
+
+        for index in range(0, 6):
+            curve.append_point(points[index])
+
+        self.workspace.add_Bezier_curve_object(curve)
+        self.workspace.change_curve_visibility(curve)
+        self.workspace.change_curve_derivative_visibility(curve, 2)
+        self.workspace.change_curve_derivative_visibility(curve, 1)
+        self.workspace.change_control_polygon_visibility(curve)
+        self.workspace.updateGL()
+
+    def open_file(self):
+        file_name = QtGui.QFileDialog.getOpenFileName(self, 'Open File',
+                                                      '.')
+        file_ = open(file_name)
+        data = file_.read()
+        #read from file and put the points in the right place
+        fname.close()
+
+    def save_file(self):
+        file_name = QtGui.QFileDialog.getSaveFileName(self, 'Save File',
+                                                      '.')
+        file_ = open(file_name, 'w')
+        #write inserted points into file
+        fname.close()
+
+    def zoom_in(self):
+        pass
+
+    def zoom_out(self):
+        pass
 
     def window_properties(self):
         self.setGeometry(1000, 640, 300, 300)
         self.setWindowTitle('3D Spline visualisation')
 
         menu_bar = self.menuBar()
-        file = menu_bar.addMenu('&File')
-        file.addSeparator()
-        self.create_menu_item('New', None, 'Ctrl+N', 'New workspace',
-                              self.workspace.reset, file)
-        self.create_menu_item('Exit', None, 'Ctrl+Q', 'Exit application',
-                              QtCore.SLOT('close()'), file)
-        # to add more menu items
+        file_ = menu_bar.addMenu('&File')
+        file_.addSeparator()
+        file_new = file_.addMenu('&New')
+        file_new.addSeparator()
 
-        #self.status_bar = QtGui.statusBar()
-        #self.status_bar.showMessage('Ready')
+        self.create_menu_item('Bezier curve', None, 'Ctrl+N', 'New workspace',
+                              self.workspace.reset, file_new)
+        self.create_menu_item('Spline', None, 'Ctrl+N', 'New workspace',
+                              self.workspace.reset, file_new)
+
+        self.create_menu_item('Open', None, 'Ctrl+O', 'Open file',
+                              self.open_file, file_)
+        self.create_menu_item('Save', None, 'Ctrl+S', 'Save file',
+                              self.save_file, file_)
+        self.create_menu_item('Exit', None, 'Ctrl+Q', 'Exit application',
+                              QtCore.SLOT('close()'), file_)
+
+        view = menu_bar.addMenu('&View')
+        view.addSeparator()
+        self.create_menu_item('Zoom In', None, 'Ctrl+', 'Zoom in workspace',
+                              self.zoom_in, view)
+        self.create_menu_item('Zoom Out', None, 'Ctrl-', 'Zoom out workspace',
+                              self.zoom_out, view)
+
+        self.status_bar = self.statusBar()
+        self.status_bar.showMessage('Ready')
 
     def environment(self):
         self.workspace = Workspace.Workspace()
--- Workspace.py
+++ Workspace.py
@@ -9,6 +9,7 @@ class Workspace(QtOpenGL.QGLWidget):
     OBJECT_SPLINESC0 = "Splines C0"
     OBJECT_SPLINESC1 = "Splines C1"
     OBJECT_SPLINESC2 = "Splines C2"
+    AXIS_LENGTH = 10
 
     def __init__(self):
         super(Workspace, self).__init__()
@@ -17,17 +18,17 @@ class Workspace(QtOpenGL.QGLWidget):
                         self.OBJECT_SPLINESC1: [],
                         self.OBJECT_SPLINESC2: []}
 
-    def add_splineC0_object(self, object):
-        self.objects[self.OBJECT_SPLINESC0].append(object)
+    def add_splineC0_object(self, object_):
+        self.objects[self.OBJECT_SPLINESC0].append(object_)
 
-    def add_splineC1_object(self, object):
-        self.objects[self.OBJECT_SPLINESC1].append(object)
+    def add_splineC1_object(self, object_):
+        self.objects[self.OBJECT_SPLINESC1].append(object_)
 
-    def add_splineC2_object(self, object):
-        self.objects[self.OBJECT_SPLINESC2].append(object)
+    def add_splineC2_object(self, object_):
+        self.objects[self.OBJECT_SPLINESC2].append(object_)
 
-    def add_Bezier_curve_object(self, object):
-        self.objects[self.OBJECT_BEZIER_CURVES].append(object)
+    def add_Bezier_curve_object(self, object_):
+        self.objects[self.OBJECT_BEZIER_CURVES].append(object_)
 
     def reset(self):
         print(self)
@@ -72,37 +73,76 @@ class Workspace(QtOpenGL.QGLWidget):
         glFlush()
 
         # Color in RGB
-        glColor3f(1.0, 1.0, 1.0)
+        glRotatef(30, 1, 0, 0)
+        glRotatef(-40, 0, 1, 0)
         glLineWidth(2)
+        glShadeModel(GL_SMOOTH)
 
         self.draw_Bezier_curve()
         self.draw_splineC0()
         self.draw_splineC1()
         self.draw_splineC2()
 
+        self.draw_derivative()
+        self.draw_control_polygon()
+
+        self.draw_coordinate_system()
+
+    def draw_coordinate_axis(self, x, y, z, red, green, blue):
+        glColor3f(red, green, blue)
+        glLineWidth(3)
+
+        glBegin(GL_LINES)
+        glVertex3f(0, 0, 0)
+        glVertex3f(x, y, z)
+        glEnd()
+
+    def draw_coordinate_system(self):
+        self.draw_coordinate_axis(self.AXIS_LENGTH, 0, 0, 1.0, 0.0, 0.0)
+        self.draw_coordinate_axis(0, self.AXIS_LENGTH, 0, 0.0, 1.0, 0.0)
+        self.draw_coordinate_axis(0, 0, self.AXIS_LENGTH, 0.0, 0.0, 1.0)
+
+    def change_curve_visibility(self, object_):
+        object_.to_be_drawn = not object_.to_be_drawn
+
+    def change_control_polygon_visibility(self, object_):
+        object_.control_polygon_to_be_drawn = (
+            not object_.control_polygon_to_be_drawn)
+
+    def change_curve_derivative_visibility(self, object_, degree):
+        object_.derivatives_to_be_drawn.add(degree)
+
     def drawGL(self, points, modeGL):
-        glBegin(mode)
+        glBegin(modeGL)
         for point in points:
-            glVertex3f(point)  # point is Vec3D -> to return a tuple?
+            glVertex3f(point.x, point.y, point.z)
         glEnd()
 
     def object_to_be_drawn(self, object_key):
-        for object in self.objects[object_key]:
-            return object.to_be_drawn and object
+        for object_ in self.objects[object_key]:
+            return object_.to_be_drawn and object_
 
-    def draw_object(self, object_key, modeGL=GL_LINE_STRIP):
-        object = self.object_to_be_drawn(object_key)
-        if object:
-            self.drawGL(object.draw(), modeGL)
+    def draw_object(self, object_key, modeGL=GL_POINTS):
+        object_ = self.object_to_be_drawn(object_key)
+        if object_:
+            print("draw!")
+            self.drawGL(object_.draw(), modeGL)
+
+    def draw_control_polygon(self):
+        glColor3f(0.3, 0.3, 0.1)
+        for object_ in self.objects[self.OBJECT_BEZIER_CURVES]:
+            self.drawGL(object_.control_points, GL_LINE_STRIP)
 
     def draw_derivative(self):
+        glColor3f(1.0, 0.5, 0.5)
         #derivative_to_be_drawn is a number of the derivative to be drawn
-        for object in self.objects[self.OBJECTS_BEZIER_CURVES]:
-            if object.derivative_to_be_drawn:
-                self.drawGL(object.draw_derivative(derivative_to_be_drawn),
-                            GL_LINE_STRIP)
+        for object_ in self.objects[self.OBJECT_BEZIER_CURVES]:
+            for value in object_.derivatives_to_be_drawn:
+                self.drawGL(object_.draw_derivative(value), GL_POINTS)
 
     def draw_Bezier_curve(self):
+        glColor3f(1.0, 1.0, 0)
+
         self.draw_object(self.OBJECT_BEZIER_CURVES)
 
     def draw_splineC0(self):
